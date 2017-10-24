f87bf2fd0eccfa107331e56d65b9d983c6fa8a51
Kalilo <kalilohansmann@gmail.com>
2017-10-24 12:24:48 +0200

    First pne working!!

--- test/Colleen.c
+++ test/Colleen.c
@@ -31,33 +31,33 @@ char		*g_src[30] = { "/* ***********************************************",
 	"nclude <string.h>\n#include <stdio.h>\n#include <ctype.h>\n\nchar\t\t*g_s",
 	"rc[30] = { ",
 	" };\n\nchar\tis_special(char c)\n{\n\treturn (c == '\\\"' || c == '\\t' |",
-	"| c == '\\n' || c == '\"');\n}\n\nchar\t*escape_special(char c)\n{\n\tif ",
-	"(c == '\\\"')\n\t\treturn (\"\\\\\");\n\tif (c == '\\t')\n\t\treturn (\"",
-	"\\\\t\");\n\tif (c == '\\n')\n\t\treturn (\"\\\\n\");\n\treturn (\"\\\\\\",
-	"\"\");\n}\n\nint\t\tmain(void)\n{\n\tFILE\t*fd;\n\tint\t\tk;\n\tint\t\tl;",
-	"\n\tint\t\tm;\n\n\tl = -1;\n\tm = -1;\n\tfd = fopen(\"Colleen.c\", \"w\")",
-	";\n\twhile (++l < 15)\n\t\tfprintf(fd, \"%s\", g_src[l]);\n\twhile (++m <",
-	" 28)\n\t{\n\t\tfprintf(fd, \"%c\", '\"');\n\t\tk = -1;\n\t\twhile (g_src[",
-	"m][++k])\n\t\t{\n\t\t\tif (!is_special(g_src[m][k]))\n\t\t\t\tfprintf(fd,",
-	" \"%c\", g_src[m][k]);\n\t\t\telse\n\t\t\t\tfprintf(fd, \"%s\", escape_sp",
-	"ecial(g_src[m][k]));\n\t\t}\n\t\tfprintf(fd, (m + 1 == 28) ? \"\\\"\" : ",
-	"\"\\\",\\n\\t\");\n\t}\n\twhile (l < 28)\n\t\tfprintf(fd, \"%s\", g_src[l",
-	"++]);\n\treturn (0);\n}\n" };
+	"| c == '\\n' || c == '\\\\');\n}\n\nchar\t*escape_special(char c)\n{\n\ti",
+	"f (c == '\\\"')\n\t\treturn (\"\\\\\\\"\");\n\tif (c == '\\t')\n\t\tretur",
+	"n (\"\\\\t\");\n\tif (c == '\\n')\n\t\treturn (\"\\\\n\");\n\treturn (\"",
+	"\\\\\\\\\");\n}\n\nint\t\tmain(void)\n{\n\tFILE\t*fd;\n\tint\t\tk;\n\tint",
+	"\t\tl;\n\tint\t\tm;\n\n\tl = -1;\n\tm = -1;\n\tfd = fopen(\"Colleen.c\", ",
+	"\"w\");\n\twhile (++l < 15)\n\t\tfprintf(fd, \"%s\", g_src[l]);\n\twhile ",
+	"(++m < 28)\n\t{\n\t\tfprintf(fd, \"%c\", '\"');\n\t\tk = -1;\n\t\twhile (",
+	"g_src[m][++k])\n\t\t{\n\t\t\tif (!is_special(g_src[m][k]))\n\t\t\t\tfprin",
+	"tf(fd, \"%c\", g_src[m][k]);\n\t\t\telse\n\t\t\t\tfprintf(fd, \"%s\", esc",
+	"ape_special(g_src[m][k]));\n\t\t}\n\t\tfprintf(fd, (m + 1 == 28) ? \"\\\"",
+	"\" : \"\\\",\\n\\t\");\n\t}\n\twhile (l < 28)\n\t\tfprintf(fd, \"%s\", g_",
+	"src[l++]);\n\treturn (0);\n}\n" };
 
 char	is_special(char c)
 {
-	return (c == '\"' || c == '\t' || c == '\n' || c == '"');
+	return (c == '\"' || c == '\t' || c == '\n' || c == '\\');
 }
 
 char	*escape_special(char c)
 {
 	if (c == '\"')
-		return ("\\");
+		return ("\\\"");
 	if (c == '\t')
 		return ("\\t");
 	if (c == '\n')
 		return ("\\n");
-	return ("\\\"");
+	return ("\\\\");
 }
 
 int		main(void)
