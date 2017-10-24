6c81fca03634f969af42e979083e13353c44ab38
Kalilo <kalilohansmann@gmail.com>
2017-10-24 14:13:48 +0200

    Removed defines

--- ex00/Colleen.c
+++ ex00/Colleen.c
@@ -17,10 +17,6 @@
 	This program will print its own source when run.
 */
 
-#define SPECIAL_CHAR (c == '\"' || c == '\t' || c == '\n' || c == '\\')
-#define LAST_LINE 29
-#define SOURCE_LINE 17
-
 char		*g_src[30] = { "/* ***********************************************",
 	"*************************** */\n/*                                       ",
 	"                                     */\n/*                              ",
@@ -35,21 +31,19 @@ char		*g_src[30] = { "/* ***********************************************",
 	"                                    */\n/* ******************************",
 	"******************************************** */\n\n#include <fcntl.h>\n#i",
 	"nclude <stdio.h>\n\n/*\n\tThis program will print its own source when run",
-	".\n*/\n\n#define SPECIAL_CHAR (c == '\\\"' || c == '\\t' || c == '\\n' ||",
-	" c == '\\\\')\n#define LAST_LINE 29\n#define SOURCE_LINE 17\n\nchar\t\t*g",
-	"_src[30] = { ",
+	".\n*/\n\nchar\t\t*g_src[30] = { ",
 	" };\n\nchar\t*escape_special(char c)\n{\n\tif (c == '\\\"')\n\t\treturn (",
 	"\"\\\\\\\"\");\n\tif (c == '\\t')\n\t\treturn (\"\\\\t\");\n\tif (c == '",
 	"\\n')\n\t\treturn (\"\\\\n\");\n\treturn (\"\\\\\\\\\");\n}\n\nint\t\tmai",
 	"n(void)\n{\n\tint\t\tk;\n\tint\t\tl;\n\tint\t\tm;\n\tchar\tc;\n\n\t/*\n\t",
 	"\tThis program will print its own source when run.\n\t*/\n\tl = -1;\n\tm ",
-	"= -1;\n\twhile (++l < SOURCE_LINE)\n\t\tprintf(\"%s\", g_src[l]);\n\twhil",
-	"e (++m < LAST_LINE)\n\t{\n\t\tprintf(\"%c\", '\"');\n\t\tk = -1;\n\t\twhi",
-	"le (g_src[m][++k])\n\t\t{\n\t\t\tc = g_src[m][k];\n\t\t\tif (!(c == '\\\"",
-	"' || c == '\\t' || c == '\\n' || c == '\\\\'))\n\t\t\t\tprintf(\"%c\", g_",
-	"src[m][k]);\n\t\t\telse\n\t\t\t\tprintf(\"%s\", escape_special(g_src[m][k",
-	"]));\n\t\t}\n\t\tprintf((m + 1 == LAST_LINE) ? \"\\\"\" : \"\\\",\\n\\t\"",
-	");\n\t}\n\twhile (l < LAST_LINE)\n\t\tprintf(\"%s\", g_src[l++]);\n}\n" };
+	"= -1;\n\twhile (++l < 15)\n\t\tprintf(\"%s\", g_src[l]);\n\twhile (++m < ",
+	"27)\n\t{\n\t\tprintf(\"%c\", '\"');\n\t\tk = -1;\n\t\twhile (g_src[m][++k",
+	"])\n\t\t{\n\t\t\tc = g_src[m][k];\n\t\t\tif (!(c == '\\\"' || c == '\\t' ",
+	"|| c == '\\n' || c == '\\\\'))\n\t\t\t\tprintf(\"%c\", g_src[m][k]);\n\t",
+	"\t\telse\n\t\t\t\tprintf(\"%s\", escape_special(g_src[m][k]));\n\t\t}\n\t",
+	"\tprintf((m + 1 == 27) ? \"\\\"\" : \"\\\",\\n\\t\");\n\t}\n\twhile (l < ",
+	"27)\n\t\tprintf(\"%s\", g_src[l++]);\n}\n" };
 
 char	*escape_special(char c)
 {
@@ -74,9 +68,9 @@ int		main(void)
 	*/
 	l = -1;
 	m = -1;
-	while (++l < SOURCE_LINE)
+	while (++l < 15)
 		printf("%s", g_src[l]);
-	while (++m < LAST_LINE)
+	while (++m < 27)
 	{
 		printf("%c", '"');
 		k = -1;
@@ -88,8 +82,8 @@ int		main(void)
 			else
 				printf("%s", escape_special(g_src[m][k]));
 		}
-		printf((m + 1 == LAST_LINE) ? "\"" : "\",\n\t");
+		printf((m + 1 == 27) ? "\"" : "\",\n\t");
 	}
-	while (l < LAST_LINE)
+	while (l < 27)
 		printf("%s", g_src[l++]);
 }
