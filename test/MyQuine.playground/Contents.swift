2385ce88687bd5707d0ea19b66f8cecbbb0f26e4
Kalilo <kalilohansmann@gmail.com>
2017-10-31 13:36:14 +0200

    got second exercise working in swift

--- test/MyQuine.playground/Contents.swift
+++ test/MyQuine.playground/Contents.swift
@@ -1,34 +1,40 @@
 //import UIKit
 import Foundation
+import Darwin
 
 /*
  this is a comment
  */
 
 class Quine {
-    let data = ["//import UIKit\n\n/*\n this is a comment\n */\n\nclass Quine ",
-                "{\n    let data = [",
-                "]\n\n    func esc_char(c: Character) -> String {\n        if ",
-                "([\"\\n\", \"\\\\\", \"\\t\", \"\\\"\"].contains(c)) {\n     ",
-                "       let s: String = String(c)\n            if (s == \"\\n",
-                "\") {\n                return (\"\\\\n\")\n            }\n   ",
-                "         if (s == \"\\\\\") {\n                return (\"\\\\",
-                "\\\\\")\n            }\n            if (s == \"\\\"\") {\n   ",
-                "             return (\"\\\\\\\"\")\n            }\n          ",
-                "  if (s == \"\\t\") {\n                return (\"\\\\t\")\n  ",
-                "          }\n        }\n        return (String(c))\n    }\n\n",
-                "    func main() {\n        /*\n         this is a comment in ",
-                "the main\n         */\n        for k in 0...1 {\n            ",
-                "print(data[k], terminator: \"\")\n        }\n        print(\"",
-                "\\\"\", terminator: \"\")\n        for k in 0...21 {\n       ",
-                "     for l in data[k].characters.indices {\n                p",
-                "rint(esc_char(c: data[k][l]), terminator: \"\")\n            ",
-                "}\n            print(\"\\\",\\n                \\\"\", termin",
-                "ator: \"\")\n        }\n        print(\"\\\"\", terminator: ",
-                "\"\")\n        for k in 2...21 {\n            print(data[k], ",
-                "terminator: \"\")\n        }\n    }\n}\n\nlet q = Quine.init(",
-                ")\n\nq.main()",
+    let data = ["//import UIKit\nimport Foundation\nimport Darwin\n\n/*\n this",
+                " is a comment\n */\n\nclass Quine {\n    let data = [",
+                "]\n    let filename = \"Grace_kid.swift\"\n\n    func esc_cha",
+                "r(c: Character) -> String {\n        if ([\"\\n\", \"\\\\\", ",
+                "\"\\t\", \"\\\"\"].contains(c)) {\n            let s: String ",
+                "= String(c)\n            if (s == \"\\n\") {\n               ",
+                " return (\"\\\\n\")\n            }\n            if (s == \"\\",
+                "\\\") {\n                return (\"\\\\\\\\\")\n            }",
+                "\n            if (s == \"\\\"\") {\n                return (",
+                "\"\\\\\\\"\")\n            }\n            if (s == \"\\t\") {",
+                "\n                return (\"\\\\t\")\n            }\n        ",
+                "}\n        return (String(c))\n    }\n\n    func write(str: S",
+                "tring) {\n        do {\n            try str.write(toFile: fil",
+                "ename, atomically: false, encoding: String.Encoding.utf8)\n  ",
+                "      }\n        catch let error as NSError {\n            pr",
+                "int(\"Error Writing to File: \\(error)\")\n            exit(0",
+                ")\n        }\n    }\n\n    func main() {\n        var content",
+                ": String = \"\"\n\n        for k in 0...1 {\n            cont",
+                "ent += data[k]\n        }\n        content += \"\\\"\"\n     ",
+                "   for k in 0...25 {\n            for l in data[k].characters",
+                ".indices {\n                content += esc_char(c: data[k][l]",
+                ")\n            }\n            content += \"\\\",\\n          ",
+                "      \\\"\"\n        }\n        content += \"\\\"\"\n       ",
+                " for k in 2...25 {\n            content += data[k]\n        }",
+                "\n        self.write(str: content)\n    }\n}\n\nlet q = Quine",
+                ".init()\n\nq.main()\n",
                 ""]
+    let filename = "Grace_kid.swift"
     
     func esc_char(c: Character) -> String {
         if (["\n", "\\", "\t", "\""].contains(c)) {
@@ -49,55 +55,37 @@ class Quine {
         return (String(c))
     }
     
+    func write(str: String) {
+        do {
+            try str.write(toFile: filename, atomically: false, encoding: String.Encoding.utf8)
+        }
+        catch let error as NSError {
+            print("Error Writing to File: \(error)")
+            exit(0)
+        }
+    }
+    
     func main() {
-        /*
-         this is a comment in the main
-         */
+        var content: String = ""
+        
         for k in 0...1 {
-            print(data[k], terminator: "")
+            content += data[k]
         }
-        print("\"", terminator: "")
-        for k in 0...21 {
+        content += "\""
+        for k in 0...25 {
             for l in data[k].characters.indices {
-                print(esc_char(c: data[k][l]), terminator: "")
+                content += esc_char(c: data[k][l])
             }
-            print("\",\n                \"", terminator: "")
+            content += "\",\n                \""
         }
-        print("\"", terminator: "")
-        for k in 2...21 {
-            print(data[k], terminator: "")
+        content += "\""
+        for k in 2...25 {
+            content += data[k]
         }
+        self.write(str: content)
     }
 }
 
 let q = Quine.init()
 
-//q.main()
-
-//let file = open("tmp.txt")
-
-//"str".write(to:atomically:encoding:)
-//let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-
-//let dir = FileManager.default
-//print(dir.currentDirectoryPath)
-//let path = fileManager.currentDirectoryPath
-
-//let file = FileHandle.init(forWritingAtPath: "./made_by_swift.txt")
-
-//file?.write("hello world!".data(using: String.Encoding.utf8)!)
-
-//let documentDirectory = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-
-//let file: FileHandle? = FileHandle(forWritingAtPath: "swift_test.txt")
-let file: FileHandle? = FileHandle(forWritingAtPath: "swifttest.txt")
-
-if file == nil {
-    print("error opening file.");
-    exit
-}
-else {
-    let str = "this is a test string"
-    file?.write(str.data(using: .utf8)!)
-    file?.closeFile()
-}
+q.main()
